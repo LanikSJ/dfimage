27def3b72365f99a37743d92fe575d7bbef50233
Restyled.io <commits@restyled.io>
2023-07-08 02:39:43 +0000

    Restyled by black

--- entrypoint.py
+++ entrypoint.py
@@ -12,9 +12,9 @@ class MainObj:
     def __init__(self):
         super(MainObj, self).__init__()
         self.commands = []
-        self.cli = client(base_url='unix://var/run/docker.sock')
+        self.cli = client(base_url="unix://var/run/docker.sock")
         self._get_image(argv[-1])
-        self.hist = self.cli.history(self.img['RepoTags'][0])
+        self.hist = self.cli.history(self.img["RepoTags"][0])
         self._parse_history()
         self.commands.reverse()
         self._print_commands()
@@ -24,38 +24,42 @@ class MainObj:
             print(i)
 
     def _get_image(self, repo_tag_or_id):
-        repo_tag = repo_tag_or_id if ':' in repo_tag_or_id else f"{repo_tag_or_id}:latest"
+        repo_tag = (
+            repo_tag_or_id if ":" in repo_tag_or_id else f"{repo_tag_or_id}:latest"
+        )
         image_id = repo_tag_or_id.lower()
         images = self.cli.images()
         for i in images:
-            if i['Id'].split(':')[1].lower().startswith(image_id):
+            if i["Id"].split(":")[1].lower().startswith(image_id):
                 self.img = i
                 return
-            rt = i['RepoTags']
+            rt = i["RepoTags"]
             if rt and repo_tag in rt:
                 self.img = i
                 return
-        raise ImageNotFound(f'Image {repo_tag} Not Found! '
-                            f'Please Make Sure You Run docker pull {repo_tag} Beforehand.\n')
+        raise ImageNotFound(
+            f"Image {repo_tag} Not Found! "
+            f"Please Make Sure You Run docker pull {repo_tag} Beforehand.\n"
+        )
 
     def _insert_step(self, step):
         if "#(nop)" in step:
             to_add = step.split("#(nop) ")[1]
         else:
-            to_add = ("RUN {}".format(step))
+            to_add = "RUN {}".format(step)
         to_add = to_add.replace("&&", "\\\n    &&")
-        self.commands.append(to_add.strip(' '))
+        self.commands.append(to_add.strip(" "))
 
     def _parse_history(self, rec=False):
         first_tag = False
         actual_tag = False
         for i in self.hist:
-            if i['Tags']:
-                actual_tag = i['Tags'][0]
+            if i["Tags"]:
+                actual_tag = i["Tags"][0]
                 if first_tag and not rec:
                     break
                 first_tag = True
-            self._insert_step(i['CreatedBy'])
+            self._insert_step(i["CreatedBy"])
         if not rec:
             self.commands.append("FROM {}".format(actual_tag))
 
