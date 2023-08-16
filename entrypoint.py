c80d19acfd526bcbf9eb96c68c15fae08896a74a
LanikSJ <LanikSJ@users.noreply.github.com>
2023-08-15 20:32:19 -0700

    Updates To Code (#204)
    * Update Dependencies
    
    * Add GitHub Action
    
    * Restyle Updates To Code (#205)
    
    * Restyled by black
    
    * Restyled by isort
    
    * Restyled by reorder-python-imports
    
    * Restyled by yapf
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * Update Test Tag
    
    * Update GitHub Action Names
    
    * fix: the right way to use docker client in python (#209)
    
    * Restyle Updates To Code (#210)
    
    * Restyled by autopep8
    
    * Restyled by black
    
    * Restyled by pyment
    
    * Restyled by whitespace
    
    * Restyled by yapf
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * Restyle Updates To Code (#211)
    
    * Restyled by black
    
    * Restyled by pyment
    
    * Restyled by whitespace
    
    * Restyled by yapf
    
    ---------
    
    Co-authored-by: Restyled.io <commits@restyled.io>
    
    * Update dfimage-test.yml
    
    * Update Script Permissions
    
    * Update Test Command
    
    ---------
    
    Co-authored-by: restyled-io[bot] <32688539+restyled-io[bot]@users.noreply.github.com>
    Co-authored-by: Restyled.io <commits@restyled.io>
    Co-authored-by: Code-Agitator <65442016+Code-Agitator@users.noreply.github.com>

--- entrypoint.py
+++ entrypoint.py
@@ -1,61 +1,83 @@
 #!/usr/bin/python3
-
 from sys import argv
-from docker import Client
+
+from docker import client
 
 
 class ImageNotFound(Exception):
+    """ """
+
     pass
 
 
 class MainObj:
+    """ """
+
     def __init__(self):
         super(MainObj, self).__init__()
         self.commands = []
-        self.cli = Client(base_url='unix://var/run/docker.sock')
+        self.cli = client.DockerClient(base_url="unix:///var/run/docker.sock")
         self._get_image(argv[-1])
-        self.hist = self.cli.history(self.img['RepoTags'][0])
+        self.hist = self.cli.api.history(self.img["RepoTags"][0])
         self._parse_history()
         self.commands.reverse()
         self._print_commands()
 
     def _print_commands(self):
+        """ """
         for i in self.commands:
             print(i)
 
     def _get_image(self, repo_tag_or_id):
-        repo_tag = repo_tag_or_id if ':' in repo_tag_or_id else f"{repo_tag_or_id}:latest"
+        """
+
+        :param repo_tag_or_id:
+
+        """
+        repo_tag = (repo_tag_or_id
+                    if ":" in repo_tag_or_id else f"{repo_tag_or_id}:latest")
         image_id = repo_tag_or_id.lower()
-        images = self.cli.images()
+        images = self.cli.api.images()
         for i in images:
-            if i['Id'].split(':')[1].lower().startswith(image_id):
+            if i["Id"].split(":")[1].lower().startswith(image_id):
                 self.img = i
                 return
-            rt = i['RepoTags']
+            rt = i["RepoTags"]
             if rt and repo_tag in rt:
                 self.img = i
                 return
-        raise ImageNotFound(f'Image {repo_tag} Not Found! '
-                            f'Please Make Sure You Run docker pull {repo_tag} Beforehand.\n')
+        raise ImageNotFound(
+            f"Image {repo_tag} Not Found! "
+            f"Please Make Sure You Run docker pull {repo_tag} Beforehand.\n")
 
     def _insert_step(self, step):
+        """
+
+        :param step:
+
+        """
         if "#(nop)" in step:
             to_add = step.split("#(nop) ")[1]
         else:
-            to_add = ("RUN {}".format(step))
+            to_add = "RUN {}".format(step)
         to_add = to_add.replace("&&", "\\\n    &&")
-        self.commands.append(to_add.strip(' '))
+        self.commands.append(to_add.strip(" "))
 
     def _parse_history(self, rec=False):
+        """
+
+        :param rec: Default value = False)
+
+        """
         first_tag = False
         actual_tag = False
         for i in self.hist:
-            if i['Tags']:
-                actual_tag = i['Tags'][0]
+            if i["Tags"]:
+                actual_tag = i["Tags"][0]
                 if first_tag and not rec:
                     break
                 first_tag = True
-            self._insert_step(i['CreatedBy'])
+            self._insert_step(i["CreatedBy"])
         if not rec:
             self.commands.append("FROM {}".format(actual_tag))
 
