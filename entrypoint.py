613781fc8769f20767f811ec6a1de390ebbfdec5
Restyled.io <commits@restyled.io>
2023-12-23 17:59:52 +0000

    Restyled by yapf

--- entrypoint.py
+++ entrypoint.py
@@ -37,9 +37,8 @@ class MainObj:
         :param repo_tag_or_id:
 
         """
-        repo_tag = (
-            repo_tag_or_id if ":" in repo_tag_or_id else f"{repo_tag_or_id}:latest"
-        )
+        repo_tag = (repo_tag_or_id
+                    if ":" in repo_tag_or_id else f"{repo_tag_or_id}:latest")
         image_id = repo_tag_or_id.lower()
         images = self.cli.api.images()
         for i in images:
@@ -52,8 +51,7 @@ class MainObj:
                 return
         raise ImageNotFound(
             f"Image {repo_tag} Not Found! "
-            f"Please Make Sure You Run docker pull {repo_tag} Beforehand.\n"
-        )
+            f"Please Make Sure You Run docker pull {repo_tag} Beforehand.\n")
 
     def _get_layers_with_images(self):
         """ """
