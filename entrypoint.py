7834d6d2625c2a6eceb1a3e760a0720bef73c3c9
Przemysław Czerkas <pczerkas@gmail.com>
2023-12-23 18:41:07 +0100

    Fix for issue #155: FROM attribute in the output Dockerfile is the image itself

--- entrypoint.py
+++ entrypoint.py
@@ -15,9 +15,13 @@ class MainObj:
     def __init__(self):
         super(MainObj, self).__init__()
         self.commands = []
+        self.layers_with_images = {}
+        self.from_img = None
         self.cli = client.DockerClient(base_url="unix:///var/run/docker.sock")
         self._get_image(argv[-1])
         self.hist = self.cli.api.history(self.img["RepoTags"][0])
+        self._get_layers_with_images()
+        self._get_from_img()
         self._parse_history()
         self.commands.reverse()
         self._print_commands()
@@ -49,6 +53,23 @@ class MainObj:
             f"Image {repo_tag} Not Found! "
             f"Please Make Sure You Run docker pull {repo_tag} Beforehand.\n")
 
+    def _get_layers_with_images(self):
+        """ """
+        images = self.cli.api.images()
+        for i in images:
+            inspect = self.cli.api.inspect_image(i["Id"])
+            try:
+                layers = inspect["RootFS"]["Layers"]
+            except KeyError:
+                continue
+            if not layers:
+                continue
+            last_layer_id = layers[-1]
+            try:
+                self.layers_with_images[last_layer_id] = i["RepoTags"][0]
+            except IndexError:
+                pass
+
     def _insert_step(self, step):
         """
 
@@ -62,23 +83,38 @@ class MainObj:
         to_add = to_add.replace("&&", "\\\n    &&")
         self.commands.append(to_add.strip(" "))
 
-    def _parse_history(self, rec=False):
+    def _parse_history(self):
         """
 
         :param rec: Default value = False)
 
         """
-        first_tag = False
-        actual_tag = False
-        for i in self.hist:
-            if i["Tags"]:
-                actual_tag = i["Tags"][0]
-                if first_tag and not rec:
-                    break
-                first_tag = True
-            self._insert_step(i["CreatedBy"])
-        if not rec:
-            self.commands.append("FROM {}".format(actual_tag))
+        from_last_created_by = False
+        if self.from_img:
+            from_hist = self.cli.api.history(self.from_img)
+            for i in from_hist:
+                from_last_created_by = i["CreatedBy"]
+                break
 
+        for i in self.hist:
+            if from_last_created_by and i["CreatedBy"] == from_last_created_by:
+                break
+            self._insert_step(i["CreatedBy"])
+
+        if self.from_img:
+            self.commands.append("FROM {}".format(self.from_img))
+
+    def _get_from_img(self):
+        """ """
+        inspect = self.cli.api.inspect_image(self.img["Id"])
+        try:
+            layers = inspect["RootFS"]["Layers"]
+        except KeyError:
+            return
+        if layers:
+            for layer_id in layers:
+                if layer_id in self.layers_with_images:
+                    self.from_img = self.layers_with_images[layer_id]
+                    break
 
 __main__ = MainObj()
