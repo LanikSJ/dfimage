abb7023ec674f71e002cfca6f062027bebcc3d5a
Restyled.io <commits@restyled.io>
2025-01-24 13:36:06 +0000

    Restyled by shfmt

--- dfimage.sh
+++ dfimage.sh
@@ -4,7 +4,7 @@
 set -e
 python3 -m venv /app
 source /app/bin/activate
-for i in "$(pip list --outdated --format=json |jq -r '.[].name' )" ; do pip install -U "$i"; done
+for i in "$(pip list --outdated --format=json | jq -r '.[].name')"; do pip install -U "$i"; done
 pip install -e /app
 yes | pip uninstall pip
 python3 /app/dfimage.py "$1"
